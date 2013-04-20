ccfb6c61128782fe744ca698d9d6b79a12ab3fb1
bartek <bartek@bartek-K53SV.(none)>
2013-04-20 14:17:02 +0200

    pa2: 100-integer-zero passed

--- PostTokenAnalyser.cpp
+++ PostTokenAnalyser.cpp
@@ -1,4 +1,5 @@
 #include "PostTokenAnalyser.hpp"
+#include <vector>
 
 PostTokenAnalyser::PostTokenAnalyser(std::shared_ptr<IPostTokenStream> output)
     : output(output)
@@ -7,10 +8,12 @@ PostTokenAnalyser::PostTokenAnalyser(std::shared_ptr<IPostTokenStream> output)
 
 void PostTokenAnalyser::emit_whitespace_sequence()
 {
+    //ignoring
 }
 
 void PostTokenAnalyser::emit_new_line()
 {
+    //ignoring
 }
 
 void PostTokenAnalyser::emit_header_name(const string& data)
@@ -23,6 +26,8 @@ void PostTokenAnalyser::emit_identifier(const string& data)
 
 void PostTokenAnalyser::emit_pp_number(const string& data)
 {
+    std::vector<unsigned char> out(4, 0);
+    output->emit_literal(data, EFundamentalType::FT_INT, &out.front(), out.size());
 }
 
 void PostTokenAnalyser::emit_character_literal(const string& data)
@@ -51,5 +56,6 @@ void PostTokenAnalyser::emit_non_whitespace_char(const string& data)
 
 void PostTokenAnalyser::emit_eof()
 {
+    output->emit_eof();
 }
 
