73cb8e13c9a34bd0321eefb717d73220ddcd9ea5
bartek <bartek@bartek-K53SV.(none)>
2013-04-20 14:24:18 +0200

    pa2: 100-simple.t passed

--- PostTokenAnalyser.cpp
+++ PostTokenAnalyser.cpp
@@ -1,4 +1,5 @@
 #include "PostTokenAnalyser.hpp"
+#include "StandardData.hpp"
 #include <vector>
 
 PostTokenAnalyser::PostTokenAnalyser(std::shared_ptr<IPostTokenStream> output)
@@ -22,6 +23,11 @@ void PostTokenAnalyser::emit_header_name(const string& data)
 
 void PostTokenAnalyser::emit_identifier(const string& data)
 {
+    auto it = StringToTokenTypeMap.find(data);
+    if (it != StringToTokenTypeMap.end())
+        output->emit_simple(data, it->second);
+    else
+        output->emit_identifier(data);
 }
 
 void PostTokenAnalyser::emit_pp_number(const string& data)
@@ -48,6 +54,11 @@ void PostTokenAnalyser::emit_user_defined_string_literal(const string& data)
 
 void PostTokenAnalyser::emit_preprocessing_op_or_punc(const string& data)
 {
+    auto it = StringToTokenTypeMap.find(data);
+    if (it != StringToTokenTypeMap.end())
+        output->emit_simple(data, it->second);
+    else
+        output->emit_invalid(data);
 }
 
 void PostTokenAnalyser::emit_non_whitespace_char(const string& data)
